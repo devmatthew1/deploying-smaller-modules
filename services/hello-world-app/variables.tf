ae1cb98f95c2f0572271bfe1fb9a41578e4c8fd3
surelay <hooddevelopers@gmail.com>
2021-10-26 11:09:50 +0100

    newly created modules

---
+++ services/hello-world-app/variables.tf
@@ -0,0 +1,79 @@
+variable "environment" {
+  description = "The name of the environment we're deploying to"
+  type = string
+}
+
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket for the database's remote state"
+  type = string
+}
+variable "db_remote_state_key" {
+  description = "The path for the database's remote state in S3"
+  type = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 Instances to run (e.g. t2.micro)"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "min_size" {
+  description = "The minimum number of EC2 Instances in the ASG"
+  type = number
+}
+variable "max_size" {
+  description = "The maximum number of EC2 Instances in the ASG"
+  type = number
+}
+
+variable "enable_autoscaling" {
+  description = "If set to true, enable auto scaling"
+  type        = bool
+}
+
+# # ---------------------------------------------------------------------------------------------------------------------
+# # REQUIRED PARAMETERS
+# # You must provide a value for each of these parameters.
+# # ---------------------------------------------------------------------------------------------------------------------
+
+# variable "db_remote_state_bucket" {
+#   description = "The name of the S3 bucket used for the database's remote state storage"
+#   type        = string
+# }
+
+# variable "db_remote_state_key" {
+#   description = "The name of the key in the S3 bucket used for the database's remote state storage"
+#   type        = string
+# }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "ami" {
+  description = "The AMI to run in the cluster"
+  default     = "ami-0c55b159cbfafe1f0"
+  type        = string
+}
+
+variable "server_text" {
+  description = "The text the web server should return"
+  default     = "Hello, World"
+  type        = string
+}
+
+variable "custom_tags" {
+  description = "Custom tags to set on the Instances in the ASG"
+  type        = map(string)
+  default     = {}
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+
