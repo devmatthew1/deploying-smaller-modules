e88e115cc0080d7ee641accb88ee1991cd5ad34d
surelay <hooddevelopers@gmail.com>
2021-10-30 15:20:57 +0100

    added dependencies to improve module testing

--- services/hello-world-app/main.tf
+++ services/hello-world-app/main.tf
@@ -73,24 +73,7 @@ resource "aws_lb_listener_rule" "asg" {
 }
 
 
-data "terraform_remote_state" "db" {
-  backend = "s3"
 
-  config = {
-    bucket = var.db_remote_state_bucket
-    key    = var.db_remote_state_key
-    region = "us-east-2"
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
-  
-}
 
 
 
--- services/hello-world-app/variables.tf
+++ services/hello-world-app/variables.tf
@@ -1,51 +1,27 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+
 variable "environment" {
   description = "The name of the environment we're deploying to"
-  type = string
-}
-
-variable "db_remote_state_bucket" {
-  description = "The name of the S3 bucket for the database's remote state"
-  type = string
-}
-variable "db_remote_state_key" {
-  description = "The path for the database's remote state in S3"
-  type = string
-}
-
-variable "instance_type" {
-  description = "The type of EC2 Instances to run (e.g. t2.micro)"
   type        = string
-  default     = "t2.micro"
 }
 
 variable "min_size" {
   description = "The minimum number of EC2 Instances in the ASG"
-  type = number
+  type        = number
 }
+
 variable "max_size" {
   description = "The maximum number of EC2 Instances in the ASG"
-  type = number
+  type        = number
 }
 
 variable "enable_autoscaling" {
   description = "If set to true, enable auto scaling"
   type        = bool
 }
-
-# # ---------------------------------------------------------------------------------------------------------------------
-# # REQUIRED PARAMETERS
-# # You must provide a value for each of these parameters.
-# # ---------------------------------------------------------------------------------------------------------------------
-
-# variable "db_remote_state_bucket" {
-#   description = "The name of the S3 bucket used for the database's remote state storage"
-#   type        = string
-# }
-
-# variable "db_remote_state_key" {
-#   description = "The name of the key in the S3 bucket used for the database's remote state storage"
-#   type        = string
-# }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
@@ -54,8 +30,14 @@ variable "enable_autoscaling" {
 
 variable "ami" {
   description = "The AMI to run in the cluster"
-  default     = "ami-0c55b159cbfafe1f0"
   type        = string
+  default     = "ami-0c55b159cbfafe1f0"
+}
+
+variable "instance_type" {
+  description = "The type of EC2 Instances to run (e.g. t2.micro)"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "server_text" {
@@ -64,16 +46,47 @@ variable "server_text" {
   type        = string
 }
 
-variable "custom_tags" {
-  description = "Custom tags to set on the Instances in the ASG"
-  type        = map(string)
-  default     = {}
-}
-
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
   default     = 8080
 }
 
+variable "custom_tags" {
+  description = "Custom tags to set on the Instances in the ASG"
+  type        = map(string)
+  default     = {}
+}
 
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket for the DB's Terraform state"
+  type        = string
+  default     = null
+}
+
+variable "db_remote_state_key" {
+  description = "The path in the S3 bucket for the DB's Terraform state"
+  type        = string
+  default     = null
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC to deploy into"
+  type        = string
+  default     = null
+}
+
+variable "subnet_ids" {
+  description = "The IDs of the subnets to deploy into"
+  type        = list(string)
+  default     = null
+}
+
+variable "mysql_config" {
+  description = "The config for the MySQL DB"
+  type        = object({
+    address = string
+    port    = number
+  })
+  default     = null
+}
